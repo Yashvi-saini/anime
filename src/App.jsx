fb3abdc8c47c31e5f33b1aeadc2bbd000decf745
Yashvi-saini <yashvisaini.ys@gmail.com>
2026-01-17 21:21:19 +0530

    footer , contact us

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import About from './components/About.jsx'
 import Features from './components/Features.jsx'
+import Story from './components/Story.jsx'
+import Contact from './components/Contact.jsx'
+import Footer from './components/Footer.jsx'
 const App =()=>{
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
@@ -9,6 +12,9 @@ const App =()=>{
       <Hero/>
       <About/>
       <Features/>
+      <Story/>
+      <Contact/>
+      <Footer/>
   </main>
   )
 }
